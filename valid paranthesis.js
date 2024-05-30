a418b5a47df06031e93f1b3f67359a198c486367
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-30 22:56:26 +0530

    Update valid paranthesis.js

--- valid paranthesis.js
+++ valid paranthesis.js
@@ -1,1 +1,25 @@
 
+var isValid = function (s) {
+
+    var stack = [];
+    var mapping = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (var i = 0; i < s.length; i++) {
+        var char = s.charAt(i);
+
+        if (char === '(' || char === '[' || char === '{') {
+            stack.push(char);
+        } else {
+            if (stack.length === 0 || stack.pop() !== mapping[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+
+};
