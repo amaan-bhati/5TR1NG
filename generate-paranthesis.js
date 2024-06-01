4bb4e71a3395590e5832ddfab76486e312f77810
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-01 23:06:06 +0530

    Update generate-paranthesis.js

--- generate-paranthesis.js
+++ generate-paranthesis.js
@@ -1,1 +1,21 @@
+var generateParenthesis = function (n) {
+    let result = [];
 
+    const backtrack = (curr, open, close) => {
+        if (curr.length === n * 2) {
+            result.push(curr);
+            return;
+        }
+
+        if (open < n) {
+            backtrack(curr + '(', open + 1, close);
+        }
+
+        if (close < open) {
+            backtrack(curr + ')', open, close + 1);
+        }
+    };
+
+    backtrack('', 0, 0);
+    return result;
+};
