b3064a1d95bae9229e3e34e0ac27f3381fa1b70d
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-01 23:07:18 +0530

    Update count-and-say.js

--- count-and-say.js
+++ count-and-say.js
@@ -1,1 +1,21 @@
+var countAndSay = function (n) {
+    if (n === 1) {
+        return "1";
+    }
+
+    var prev = countAndSay(n - 1);
+    var count = 1;
+    var say = "";
+
+    for (var i = 0; i < prev.length; i++) {
+        if (prev[i] === prev[i + 1]) {
+            count++;
+        } else {
+            say += count + prev[i];
+            count = 1;
+        }
+    }
+
+    return say;
+}
 
